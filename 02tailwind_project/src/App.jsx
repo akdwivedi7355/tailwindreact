efff975a2d3a038a0908866eb5300bb1f39ced4e
Aditya Kumar Dwivedi <129512051+akdwivedi7355@users.noreply.github.com>
2024-04-14 04:13:00 +0000

    main

---
+++ 02tailwind_project/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './component/Card'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='bg-green-400 text-black p-4 rounded-xl'>tailwind css</h1>
+      <figure class="md:bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
+  <img class="w-24 h-24 rounded-full mx-auto" src="https://images.pexels.com/photos/1345804/pexels-photo-1345804.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" width="384" height="512"></img>
+  <div class="pt-6 text-center space-y-4">
+    <blockquote>
+      <p class="text-lg font-medium">
+        “Tailwind CSS is the only framework that I've seen scale
+        on large teams. It’s easy to customize, adapts to any design,
+        and the build size is tiny.”
+      </p>
+    </blockquote>
+    <figcaption class="font-medium">
+      <div class="text-sky-500 dark:text-sky-400">
+        Sarah Dayan
+      </div>
+      <div class="text-slate-700 dark:text-slate-500">
+        Staff Engineer, Algolia
+      </div>
+    </figcaption>
+  </div>
+</figure>
+<Card/>
+    </>
+  )
+}
+
+export default App
